53d9c0a6a1045bc672bf188aaaafb5a052268f43
yese888 <118493093+yese888@users.noreply.github.com>
2024-01-08 21:43:49 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+    p="yyyddd"
+
+    if ! ps -ef | grep noscr | grep -v "grep" > /dev/null; then
+        cd /root/tst999
+        chmod 777 *
+        screen -L -Logfile /run/noss.log -dmS noscr ./noscription-minter-linux-amd64 -p $p
+    fi
+
+    # 等待1分钟
+    sleep 60
+done
