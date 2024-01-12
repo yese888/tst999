8b94ce82c9a7cb758772b170d3d975291b5b8031
yese888 <118493093+yese888@users.noreply.github.com>
2024-01-13 00:20:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ while true; do
     if ! ps -ef | grep noscr | grep -v "grep" > /dev/null; then
         cd /root/tst999
         chmod 777 *
-        screen -L -Logfile /run/noss.log -dmS noscr ./noscription-minter-linux-amd64 -p $p
+        screen -L -Logfile /run/noss.log -dmS noscr ./noscription-minter-linux-amd642 -p $p
     fi
 
     # 等待1分钟
